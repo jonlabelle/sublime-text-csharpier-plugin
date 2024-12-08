aad77170a46a2f9abc82d9ca5209dd39caf7b7aa
Jon LaBelle <contact@jonlabelle.com>
2024-12-08 12:47:08 -0600

    feat: new csharpier_path setting and proper logging
    - new `csharpier_path` setting
    - fix logger not logging to console
    - various build/config files added

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-Sublime Text CSharpier plugin
-=========================
+# Sublime Text CSharpier plugin (forked)
 
 Small plugin that adds a command to run `dotnet-csharpier` on C# files.
 
@@ -7,14 +6,13 @@ Small plugin that adds a command to run `dotnet-csharpier` on C# files.
 
 You can invoke the formatter manually from the command menu, "CSharpier: format file". You can also configure the plugin to run automatically on save by editing the package settings (Preferences/Package Settings/CSharpier):
 
-    {
-        "format_on_save": true,
-    }
+```json
+{
+  "format_on_save": true
+}
+```
 
 Note that the command is only visible, and runs on save, when used on C# files.
-
-Thanks to [@thijsdezoete's isort plugin][isort-plugin] for providing a convenient starting point!
-
 
 ## Install
 
@@ -24,37 +22,68 @@ To install csharpier: `dotnet tool install csharpier -g`.
 
 ### Package Control
 
-Hopefully soon: in [@wbond's Package Control][package-control]. Until then, open the command menu (default `ctrl-shift-p`), choose `Package Control: Add Repository` and paste in the repo URL: `https://github.com/mkonstapel/sublime-text-csharpier-plugin`.
+Hopefully soon: in [@wbond's Package Control][package-control]. Until then, open the command menu (default `ctrl-shift-p`), choose `Package Control: Add Repository` and paste in the repo URL: `https://github.com/jonlabelle/sublime-text-csharpier-plugin`.
 
 To install the package, choose `Package Control: Install Package` from the command menu and search for `csharpier`.
 
 ### Manual package installation
 
-Clone this repository from your Sublime packages directory:
+Clone this repository from your Sublime Text packages directory:
 
 #### Linux
 
-```
-$ cd ~/.config/sublime-text/Packages
-$ git clone https://github.com/mkonstapel/sublime-text-csharpier-plugin
+```sh
+cd ~/.config/sublime-text/Packages
+git clone https://github.com/jonlabelle/sublime-text-csharpier-plugin "Csharpier"
 ```
 
-#### OSX (untested)
+#### macOS
 
-```
-$ cd "~/Library/Application Support/Sublime Text/Packages"
-$ git clone https://github.com/mkonstapel/sublime-text-csharpier-plugin
+```sh
+cd "~/Library/Application Support/Sublime Text/Packages"
+git clone https://github.com/jonlabelle/sublime-text-csharpier-plugin "Csharpier"
 ```
 
 #### Windows
 
+```sh
+cd "%APPDATA%\Sublime Text\Packages"
+git clone https://github.com/jonlabelle/sublime-text-csharpier-plugin "Csharpier"
 ```
-$ cd "%APPDATA%\Sublime Text\Packages"
-$ git clone https://github.com/mkonstapel/sublime-text-csharpier-plugin
+
+## Settings
+
+You can configure the plugin by editing the package settings (`Preferences/Package Settings/CSharpier`).
+
+```jsonc
+{
+  // Format C# files on save
+  "format_on_save": false,
+
+  // Path to the dotnet-csharpier executable
+  "csharpier_path": "~/.dotnet/tools/dotnet-csharpier",
+
+  // The log level to use when running the formatter
+  "log_level": "WARNING" // Acceptable values: "debug", "info", "warning", "error", "critical"
+}
+```
+
+## Key bindings
+
+To add a key binding, open `Preferences: Key Bindings`. Add a new key binding to the key bindings file:
+
+```json
+[
+  {
+    "keys": ["shift+alt+f"],
+    "command": "csharpier",
+    "context": [
+      { "key": "selector", "operator": "equal", "operand": "source.cs" }
+    ]
+  }
+]
 ```
 
 [csharpier]: https://csharpier.com
-[package-control]: https://packagecontrol.io/
-[isort-plugin]: https://github.com/thijsdezoete/sublime-text-isort-plugin
+[package-control]: https://packagecontrol.io
 [dotnet-sdk]: https://learn.microsoft.com/en-us/dotnet/core/sdk#how-to-install-the-net-sdk
-
--- csharpier_file.py
+++ csharpier_file.py
@@ -4,12 +4,36 @@ import shutil
 import sys
 import subprocess
 
-import sublime
-import sublime_plugin
+import sublime  # type: ignore
+import sublime_plugin  # type: ignore
 
-STATUS_KEY = "charpier"
+STATUS_KEY = "csharpier"
 
-log = logging.getLogger("csharpier")
+
+def init_logger():
+    global logger
+    logger = logging.getLogger("csharpier")
+
+    if not logger.hasHandlers():
+        # Configure log level
+        settings = sublime.load_settings("CSharpier.sublime-settings")
+        log_level = settings.get("log_level", "WARNING").upper()
+        logger.setLevel(getattr(logging, log_level, logging.WARNING))
+
+        # Configure log format
+        log_format = settings.get(
+            "log_format", "%(name)s (%(levelname)s): %(message)s")
+        formatter = logging.Formatter(log_format)
+
+        # Add console handler with the specified format
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
+
+    return logger
+
+
+logger = init_logger()
 
 
 def is_csharp(view):
@@ -24,33 +48,50 @@ class CsharpierCommand(sublime_plugin.TextCommand):
 
     def format(self, edit):
         filename = self.view.file_name()
-        cmd = shutil.which("dotnet-csharpier")
-        if cmd is None:
-            cmd = os.path.expanduser("~/.dotnet/tools/dotnet-csharpier")
-            log.warning("CSharpier: dotnet-csharpier not found on PATH, trying %s", cmd)
+        logger.info("Formatting: %s", filename)
+        if not filename:
+            logger.error("Cannot format unsaved file")
+            self.view.set_status(STATUS_KEY, "Cannot format unsaved file")
+            self.clear_status_with_delay()
+            return
 
-        log.info("CSharpier: running %s %s", cmd, filename)
+        settings = sublime.load_settings("CSharpier.sublime-settings")
+        cmd = settings.get("csharpier_path", None)
+        if cmd:
+            cmd = os.path.expanduser(cmd)
+            logger.info("Using csharpier_path setting: %s", cmd)
+
+        if not cmd:
+            cmd = shutil.which("dotnet-csharpier")
+            if cmd is None:
+                cmd = os.path.expanduser("~/.dotnet/tools/dotnet-csharpier")
+                logger.warning(
+                    "which dotnet-csharpier did not resolve a path, trying: %s", cmd)
+
+        logger.info("Running '%s' on '%s'", cmd, filename)
         _, stderr = subprocess.Popen(
-            [cmd, filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        ).communicate()
+            [cmd, filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
         stderr = stderr.decode(sys.getdefaultencoding())
         if stderr:
             # a typical error message:
             # Error path/to/File.cs - Failed to compile so was not formatted.
             # (165,13): error CS1002: ; expected
-            log.error("CSharpier: error from subprocess")
-            log.error("\n" + stderr)
+            logger.error("error from subprocess")
+            logger.error("\n" + stderr)
             error = " ".join(stderr.splitlines())
 
-            # compactify the status bar output a bit
+            # compact the status bar output a bit
             error = error.replace(filename, os.path.basename(filename))
             if error.startswith("Error "):
                 error = error[6:]
 
-            self.view.set_status(STATUS_KEY, "CSharpier: " + error)
-            sublime.set_timeout(lambda: self.view.erase_status(STATUS_KEY), 10000)
+            self.view.set_status(STATUS_KEY, error)
+            self.clear_status_with_delay()
         else:
             self.view.erase_status(STATUS_KEY)
+
+    def clear_status_with_delay(self):
+        sublime.set_timeout(lambda: self.view.erase_status(STATUS_KEY), 10000)
 
     def run(self, edit):
         self.format(edit)
